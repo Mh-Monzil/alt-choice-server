089c1d44e403226c949905a654ec9de2e63be6d0
Mh-Monzil <monzil246@gmail.com>
2024-05-13 16:32:19 +0600

    increment recommendation count added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
 // altchoice11
 // eDed63MbFMjZ8kaB
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6yhdqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,71 +19,106 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const queryCollection = client.db('AltChoiceDB').collection('query');
-    const recommendationCollection = client.db('AltChoiceDB').collection('recommendation');
+    const queryCollection = client.db("AltChoiceDB").collection("query");
+    const recommendationCollection = client
+      .db("AltChoiceDB")
+      .collection("recommendation");
 
     //get all query data
-    app.get('/query', async (req, res) => {
+    app.get("/query", async (req, res) => {
       const result = await queryCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     //get my query by email
-    app.get('/my-query/:email', async (req, res) => {
+    app.get("/my-query/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {'queryUser.email' : email};
+      const query = { "queryUser.email": email };
       const result = await queryCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     //get single query by id
-    app.get('/query/:id', async (req, res) => {
+    app.get("/query/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await queryCollection.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
+
+    //get all recommendations
+    app.get("/recommendations", async (req, res) => {
+      const result = await recommendationCollection.find().toArray();
+      res.send(result);
+    });
 
     //post all query
-    app.post('/query', async (req, res) => {
-        const queryData = req.body;
-        const result = await queryCollection.insertOne(queryData)
-        res.send(result);
-    })
+    app.post("/query", async (req, res) => {
+      const queryData = req.body;
+      const result = await queryCollection.insertOne(queryData);
+      res.send(result);
+    });
 
-    //update query 
-    app.put('/update-query/:id', async (req, res) => {
+    //post all recommendations
+    app.post("/recommendations", async (req, res) => {
+      const recommendedData = req.body;
+      const result = await recommendationCollection.insertOne(recommendedData);
+      res.send(result);
+    });
+    // increment recommendation count
+    app.post("/increment/:id", async (req, res) => {
       const id = req.params.id;
-      const queryData = req.body
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      console.log(id);
+      const result = await queryCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $inc: {
+            recommendationCount: 1,
+          },
+        }
+      );
+      res.status(200).json(result);
+    });
+
+    //update query
+    app.put("/update-query/:id", async (req, res) => {
+      const id = req.params.id;
+      const queryData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
-          ...queryData
-        }
-      }
-      const result = await queryCollection.updateOne(filter,updateDoc, options);
+          ...queryData,
+        },
+      };
+      const result = await queryCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
-    })
+    });
 
     //delete query
-    app.delete('/delete-query/:id', async (req, res) => {
+    app.delete("/delete-query/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await queryCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -93,9 +126,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Alternative choice is running');
-})
+app.get("/", (req, res) => {
+  res.send("Alternative choice is running");
+});
 
 app.listen(port, () => console.log(`Hello I am active now ${port}`));
